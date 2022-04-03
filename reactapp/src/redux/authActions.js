12b02536198bfd00e40283967d7f03ef08727417
Rahul kumar paswan <rahulkumarpaswan941@gmail.com>
2022-04-03 13:08:41 +0530

    react_connect4

--- reactapp/src/redux/authActions.js
+++ reactapp/src/redux/authActions.js
@@ -1,22 +1,19 @@
 import {AUTH_REQ,AUTH_SUCCESS,AUTH_FAILURE} from './types';
 
-
-
-export const authSuccess= (conte_)
-;
-
-    localStorage.setItem('USER_temp',content.token);
-    return {
-        type:AUTH_SUCCESS,
-        payload:content.AUTH_FAILURE
-    }
-}
-
 export const authenticate=()=>{
     return {
         type:AUTH_REQ
     }
 }
+
+export const authSuccess= (content)=>{
+    localStorage.setItem('USER_KEY',content.token);
+    return {
+        type:AUTH_SUCCESS,
+        payload:content
+    }
+}
+
 export const authFailure=(error)=>{
     return {
         type:AUTH_FAILURE,
